2d4456721e3ee126ca56c63f969e4785bb41364f
AHMED NABIB <nabibpallab@gmail.com>
2022-09-21 12:47:18 +0600

    registration complete

--- register.php
+++ register.php
@@ -1,3 +1,30 @@
+<?php 
+  include 'config.php';
+
+
+  if (isset($_POST['submit'])) {
+
+     $name = mysqli_real_escape_string($conn,$_POST['name']);
+  $email = mysqli_real_escape_string($conn,$_POST['email']);
+  $pass = mysqli_real_escape_string($conn,$_POST['password']);
+  $cpass = mysqli_real_escape_string($conn,$_POST['cpassword']);
+
+  $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $message[] = 'user already exist!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `user_form`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
+      $message[] = 'registered successfully!';
+      //header('location:login.php');
+   }
+    
+  }
+
+ 
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +38,14 @@
 
 </head>
 <body>
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
+   }
+}
+?>
     <div class="form-container">
           <form action="" method="post">
       <h3>register now</h3>
